f2d1f16ec9222e219db1c252235e0f85d75691a3
Yadnesh_Patil <109797704+Yadnesh-Patil@users.noreply.github.com>
2024-07-12 23:10:16 +0530

    Update settings.py

--- CV/settings.py
+++ CV/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = 'django-insecure-_f0p@m_gtf-6d&gi(_r5@yt^)tah#a7qk(bdo_45pche2b98$1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', ]
+# '.now.sh'
 
 
 # Application definition
